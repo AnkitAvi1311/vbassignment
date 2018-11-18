1aec4967a3a75e56712722ab7247ab4b4c7d4b02
AnkitSingh <ankitavi11@outlook.com>
2018-11-18 14:32:40 +0530

    First commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,355 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Dim IDLabel As System.Windows.Forms.Label
+        Dim FnameLabel As System.Windows.Forms.Label
+        Dim EmailLabel As System.Windows.Forms.Label
+        Dim ContactLabel As System.Windows.Forms.Label
+        Dim StateLabel As System.Windows.Forms.Label
+        Dim CityLabel As System.Windows.Forms.Label
+        Me.MyappDataSet = New WindowsApp2.myappDataSet()
+        Me.UserBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.UserTableAdapter = New WindowsApp2.myappDataSetTableAdapters.userTableAdapter()
+        Me.TableAdapterManager = New WindowsApp2.myappDataSetTableAdapters.TableAdapterManager()
+        Me.UserBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
+        Me.UserBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
+        Me.IDTextBox = New System.Windows.Forms.TextBox()
+        Me.FnameTextBox = New System.Windows.Forms.TextBox()
+        Me.EmailTextBox = New System.Windows.Forms.TextBox()
+        Me.ContactTextBox = New System.Windows.Forms.TextBox()
+        Me.StateTextBox = New System.Windows.Forms.TextBox()
+        Me.CityTextBox = New System.Windows.Forms.TextBox()
+        IDLabel = New System.Windows.Forms.Label()
+        FnameLabel = New System.Windows.Forms.Label()
+        EmailLabel = New System.Windows.Forms.Label()
+        ContactLabel = New System.Windows.Forms.Label()
+        StateLabel = New System.Windows.Forms.Label()
+        CityLabel = New System.Windows.Forms.Label()
+        CType(Me.MyappDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.UserBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.UserBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.UserBindingNavigator.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MyappDataSet
+        '
+        Me.MyappDataSet.DataSetName = "myappDataSet"
+        Me.MyappDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'UserBindingSource
+        '
+        Me.UserBindingSource.DataMember = "user"
+        Me.UserBindingSource.DataSource = Me.MyappDataSet
+        '
+        'UserTableAdapter
+        '
+        Me.UserTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.UpdateOrder = WindowsApp2.myappDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        Me.TableAdapterManager.userTableAdapter = Me.UserTableAdapter
+        '
+        'UserBindingNavigator
+        '
+        Me.UserBindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
+        Me.UserBindingNavigator.BindingSource = Me.UserBindingSource
+        Me.UserBindingNavigator.CountItem = Me.BindingNavigatorCountItem
+        Me.UserBindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
+        Me.UserBindingNavigator.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.UserBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.UserBindingNavigatorSaveItem})
+        Me.UserBindingNavigator.Location = New System.Drawing.Point(0, 0)
+        Me.UserBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
+        Me.UserBindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
+        Me.UserBindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.UserBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.UserBindingNavigator.Name = "UserBindingNavigator"
+        Me.UserBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
+        Me.UserBindingNavigator.Size = New System.Drawing.Size(712, 27)
+        Me.UserBindingNavigator.TabIndex = 0
+        Me.UserBindingNavigator.Text = "BindingNavigator1"
+        '
+        'BindingNavigatorMoveFirstItem
+        '
+        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
+        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(24, 22)
+        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(24, 22)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 25)
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 27)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(45, 20)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 6)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(24, 24)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'BindingNavigatorMoveLastItem
+        '
+        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
+        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(24, 24)
+        Me.BindingNavigatorMoveLastItem.Text = "Move last"
+        '
+        'BindingNavigatorSeparator2
+        '
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 6)
+        '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(24, 24)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(24, 24)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
+        'UserBindingNavigatorSaveItem
+        '
+        Me.UserBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.UserBindingNavigatorSaveItem.Image = CType(resources.GetObject("UserBindingNavigatorSaveItem.Image"), System.Drawing.Image)
+        Me.UserBindingNavigatorSaveItem.Name = "UserBindingNavigatorSaveItem"
+        Me.UserBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 23)
+        Me.UserBindingNavigatorSaveItem.Text = "Save Data"
+        '
+        'IDLabel
+        '
+        IDLabel.AutoSize = True
+        IDLabel.Location = New System.Drawing.Point(17, 61)
+        IDLabel.Name = "IDLabel"
+        IDLabel.Size = New System.Drawing.Size(25, 17)
+        IDLabel.TabIndex = 1
+        IDLabel.Text = "ID:"
+        '
+        'IDTextBox
+        '
+        Me.IDTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.UserBindingSource, "ID", True))
+        Me.IDTextBox.Location = New System.Drawing.Point(81, 58)
+        Me.IDTextBox.Name = "IDTextBox"
+        Me.IDTextBox.Size = New System.Drawing.Size(159, 22)
+        Me.IDTextBox.TabIndex = 2
+        '
+        'FnameLabel
+        '
+        FnameLabel.AutoSize = True
+        FnameLabel.Location = New System.Drawing.Point(17, 89)
+        FnameLabel.Name = "FnameLabel"
+        FnameLabel.Size = New System.Drawing.Size(51, 17)
+        FnameLabel.TabIndex = 3
+        FnameLabel.Text = "fname:"
+        '
+        'FnameTextBox
+        '
+        Me.FnameTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.UserBindingSource, "fname", True))
+        Me.FnameTextBox.Location = New System.Drawing.Point(81, 86)
+        Me.FnameTextBox.Name = "FnameTextBox"
+        Me.FnameTextBox.Size = New System.Drawing.Size(159, 22)
+        Me.FnameTextBox.TabIndex = 4
+        '
+        'EmailLabel
+        '
+        EmailLabel.AutoSize = True
+        EmailLabel.Location = New System.Drawing.Point(17, 117)
+        EmailLabel.Name = "EmailLabel"
+        EmailLabel.Size = New System.Drawing.Size(45, 17)
+        EmailLabel.TabIndex = 5
+        EmailLabel.Text = "email:"
+        '
+        'EmailTextBox
+        '
+        Me.EmailTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.UserBindingSource, "email", True))
+        Me.EmailTextBox.Location = New System.Drawing.Point(81, 114)
+        Me.EmailTextBox.Name = "EmailTextBox"
+        Me.EmailTextBox.Size = New System.Drawing.Size(159, 22)
+        Me.EmailTextBox.TabIndex = 6
+        '
+        'ContactLabel
+        '
+        ContactLabel.AutoSize = True
+        ContactLabel.Location = New System.Drawing.Point(17, 145)
+        ContactLabel.Name = "ContactLabel"
+        ContactLabel.Size = New System.Drawing.Size(58, 17)
+        ContactLabel.TabIndex = 7
+        ContactLabel.Text = "contact:"
+        '
+        'ContactTextBox
+        '
+        Me.ContactTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.UserBindingSource, "contact", True))
+        Me.ContactTextBox.Location = New System.Drawing.Point(81, 142)
+        Me.ContactTextBox.Name = "ContactTextBox"
+        Me.ContactTextBox.Size = New System.Drawing.Size(159, 22)
+        Me.ContactTextBox.TabIndex = 8
+        '
+        'StateLabel
+        '
+        StateLabel.AutoSize = True
+        StateLabel.Location = New System.Drawing.Point(17, 173)
+        StateLabel.Name = "StateLabel"
+        StateLabel.Size = New System.Drawing.Size(43, 17)
+        StateLabel.TabIndex = 9
+        StateLabel.Text = "state:"
+        '
+        'StateTextBox
+        '
+        Me.StateTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.UserBindingSource, "state", True))
+        Me.StateTextBox.Location = New System.Drawing.Point(81, 170)
+        Me.StateTextBox.Name = "StateTextBox"
+        Me.StateTextBox.Size = New System.Drawing.Size(159, 22)
+        Me.StateTextBox.TabIndex = 10
+        '
+        'CityLabel
+        '
+        CityLabel.AutoSize = True
+        CityLabel.Location = New System.Drawing.Point(17, 201)
+        CityLabel.Name = "CityLabel"
+        CityLabel.Size = New System.Drawing.Size(33, 17)
+        CityLabel.TabIndex = 11
+        CityLabel.Text = "city:"
+        '
+        'CityTextBox
+        '
+        Me.CityTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.UserBindingSource, "city", True))
+        Me.CityTextBox.Location = New System.Drawing.Point(81, 198)
+        Me.CityTextBox.Name = "CityTextBox"
+        Me.CityTextBox.Size = New System.Drawing.Size(159, 22)
+        Me.CityTextBox.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(712, 522)
+        Me.Controls.Add(IDLabel)
+        Me.Controls.Add(Me.IDTextBox)
+        Me.Controls.Add(FnameLabel)
+        Me.Controls.Add(Me.FnameTextBox)
+        Me.Controls.Add(EmailLabel)
+        Me.Controls.Add(Me.EmailTextBox)
+        Me.Controls.Add(ContactLabel)
+        Me.Controls.Add(Me.ContactTextBox)
+        Me.Controls.Add(StateLabel)
+        Me.Controls.Add(Me.StateTextBox)
+        Me.Controls.Add(CityLabel)
+        Me.Controls.Add(Me.CityTextBox)
+        Me.Controls.Add(Me.UserBindingNavigator)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.MyappDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.UserBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.UserBindingNavigator, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.UserBindingNavigator.ResumeLayout(False)
+        Me.UserBindingNavigator.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MyappDataSet As myappDataSet
+    Friend WithEvents UserBindingSource As BindingSource
+    Friend WithEvents UserTableAdapter As myappDataSetTableAdapters.userTableAdapter
+    Friend WithEvents TableAdapterManager As myappDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents UserBindingNavigator As BindingNavigator
+    Friend WithEvents BindingNavigatorAddNewItem As ToolStripButton
+    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
+    Friend WithEvents BindingNavigatorDeleteItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveFirstItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
+    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveLastItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator2 As ToolStripSeparator
+    Friend WithEvents UserBindingNavigatorSaveItem As ToolStripButton
+    Friend WithEvents IDTextBox As TextBox
+    Friend WithEvents FnameTextBox As TextBox
+    Friend WithEvents EmailTextBox As TextBox
+    Friend WithEvents ContactTextBox As TextBox
+    Friend WithEvents StateTextBox As TextBox
+    Friend WithEvents CityTextBox As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub UserBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles UserBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.UserBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.MyappDataSet)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'MyappDataSet.user' table. You can move, or remove it, as needed.
+        Me.UserTableAdapter.Fill(Me.MyappDataSet.user)
+
+    End Sub
+End Class
